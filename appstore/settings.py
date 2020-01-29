25b6532ee24b51300a794589acf0058c9f8f5d60
Nikolay Zamotaev <nzamotaev@luxoft.com>
2020-01-29 18:17:51 +0300

    Documentation and settings update for use with static files served separately
    Task-number: AUTOSUITE-1429
    Change-Id: Ib670d8c41f617808db742289a4ae4ab1d77a1919
    Reviewed-by: Egor Nemtsev <enemtsev@luxoft.com>
    (cherry picked from commit 2c6654413f952eecb3ce81579d27908438f3a507)
    Reviewed-by: Nikolay Zamotaev <nzamotaev@luxoft.com>

--- appstore/settings.py
+++ appstore/settings.py
@@ -149,6 +149,7 @@ STATIC_URL = '/static/'
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
